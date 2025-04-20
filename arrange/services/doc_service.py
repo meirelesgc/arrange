b7a0da0ff22016ed10552487184f2e19635419f1
gmcosta <geu_costa@outlook.com>
2025-04-20 20:12:50 -0300

    test: mockando o modelo de embbedings

--- arrange/services/doc_service.py
+++ arrange/services/doc_service.py
@@ -32,9 +32,32 @@ def split_documents(documents: list[Document]):
     return text_splitter.split_documents(documents)
 
 
+def index_chunks(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get('source')
+        page = chunk.metadata.get('page')
+        current_page_id = f'{source}:{page}'
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f'{current_page_id}:{current_chunk_index}'
+        last_page_id = current_page_id
+
+        chunk.metadata['id'] = chunk_id
+
+    return chunks
+
+
 async def add_doc_vectorstore(vectorstore: VectorStore, doc: doc_models.Doc):
     chunks = load_documents(doc)
     chunks = split_documents(chunks)
+    chunks = index_chunks(chunks)
     await vectorstore.aadd_documents(chunks)
 
 
