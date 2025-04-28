73a18d4e4303ae606c9b424b63e65f4ec91abfc7
meirelesgc <geu_costa@outlook.com>
2025-04-28 10:49:07 -0300

    fix: alterando o modelo

--- arrange/core/vectorstore.py
+++ arrange/core/vectorstore.py
@@ -6,7 +6,7 @@ from arrange.config import Settings
 connection_url = Settings()._get_connection_string()
 
 vectorstore = PGVector(
-    embeddings=OllamaEmbeddings(model='qwen2.5-coder:1.5b-base'),
+    embeddings=OllamaEmbeddings(model='all-minilm'),
     connection=connection_url,
     use_jsonb=True,
     async_mode=True,
