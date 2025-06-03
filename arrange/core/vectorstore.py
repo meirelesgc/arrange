14c6501f41381fcda01807c5884e98daf217f3db
meirelesgc <geu_costa@outlook.com>
2025-06-03 07:42:44 -0300

    ...

--- arrange/core/vectorstore.py
+++ arrange/core/vectorstore.py
@@ -1,6 +1,4 @@
 from langchain_ollama import OllamaEmbeddings
-
-# from langchain_openai import OpenAIEmbeddings
 from langchain_postgres import PGVector
 
 from arrange.config import Settings
@@ -8,11 +6,9 @@ from arrange.config import Settings
 connection_url = Settings()._get_connection_string()
 
 vectorstore = PGVector(
-    embeddings=OllamaEmbeddings(model='qwen2.5-coder:1.5b-base'),
-    # embeddings=OpenAIEmbeddings(
-    #     model='text-embedding-3-small',
-    #     api_key=Settings().OPENAI_API_KEY,
-    # ),
+    embeddings=OllamaEmbeddings(
+        model='qwen2.5-coder:1.5b-base', base_url=Settings().OLLAMA_BASE_URL
+    ),
     connection=connection_url,
     use_jsonb=True,
     async_mode=True,
